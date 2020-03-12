24831e86a4bf731dec62ebcbbdf1c7ba25d4c164
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-12 20:57:43 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,34 +48,44 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  ret_hash = []
+  col = 0 
+  while movies_collection[col] do 
+    ret_hash[col]= movie_with_director_name(name, movies_collection[col])  
+    col += 1
+  end
+  ret_hash
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  ret_hash = {}
+  row = 0 
+  while collection[row] do
+    name = collection[row][:studio]
+    total= collection[row][:worldwide_gross]
+    if ret_hash[name]  
+      ret_hash[name] += total
+    else
+      ret_hash[name] = total
+    end
+    row += 1 
+  end
+  ret_hash
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  index = 0
+  a_o_a_movies_by_dir = []
+
+  while index < source.length do
+    dir_info_hash = source[index]
+    director_name = dir_info_hash[:name]
+    directors_movies = dir_info_hash[:movies]
+    a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
+    index += 1
+  end
+  a_o_a_movies_by_dir
 end
 
 # ----------------    End of Your Code Region --------------------
