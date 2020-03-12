22cf7704b9dfcdbdf2f1b57a87ee5aed0f06374f
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-12 20:54:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -67,16 +67,6 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
   index = 0
   a_o_a_movies_by_dir = []
 
@@ -87,7 +77,6 @@ def movies_with_directors_set(source)
     a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
     index += 1
   end
-
   a_o_a_movies_by_dir
 end
 
